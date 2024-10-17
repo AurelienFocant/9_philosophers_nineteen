ce3718e4f45380293150a570fd61887fcc3e3aba
Aurelien Focant <24444885+AurelienFocant@users.noreply.github.com>
2024-10-17 17:59:37 +0200

    we have the concept of a plan

---
+++ include/philosophers.h
@@ -0,0 +1,17 @@
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+
+typedef	struct s_context
+{
+	int	time_to_die;
+	int	time_to_eat;
+	int	time_to_sleep;
+	int	nb_of_meals;
+}	t_context;
+
+typedef struct s_philo
+{
+	t_context	*context;
+}	t_philo;
+
+#endif
