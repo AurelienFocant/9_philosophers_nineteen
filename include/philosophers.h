e71e3cf80992e49bb4f725800b220297f4bfde73
Aurelien Focant <24444885+AurelienFocant@users.noreply.github.com>
2024-10-22 21:51:34 +0200

    finally started working here, not even sure thats how you use mutex

--- include/philosophers.h
+++ include/philosophers.h
@@ -3,6 +3,7 @@
 
 typedef	struct s_context
 {
+	int	nb_of_philo;
 	int	time_to_die;
 	int	time_to_eat;
 	int	time_to_sleep;
--- src/main.c
+++ src/main.c
@@ -1,10 +1,57 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <pthread.h>
 
+#include "libft.h"
 #include "philosophers.h"
+
+t_context	fn_setup_context(char **argv)
+{
+	t_context	context;
+
+	context.nb_of_philo = ft_atoi(argv[1]);
+	context.time_to_die = ft_atoi(argv[2]);
+	context.time_to_eat = ft_atoi(argv[3]);
+	context.time_to_sleep = ft_atoi(argv[4]);
+	if (argv[5])
+		context.nb_of_meals = ft_atoi(argv[5]);
+	return (context);
+}
+
+bool	fn_check_args(int argc)
+{
+	if (argc < 5 || argc > 6)
+		return (false);
+	return (true);
+}
+
+pthread_mutex_t	*fn_malloc_forks(t_context *context)
+{
+	pthread_mutex_t	*forks;
+	int	i;
+
+	forks = malloc(sizeof(pthread_mutex_t) * context->nb_of_philo);
+	i = 0;
+	while (i < context->nb_of_philo)
+	{
+		pthread_mutex_init(&forks[i], NULL);
+		i++;
+	}
+	return (forks);
+}
 
 int	main(int argc, char **argv)
 {
-	(void) argc;
-	(void) argv;
+	pthread_mutex_t	*forks;
+	t_context	context;
+	
+	if (!fn_check_args(argc))
+	{
+		printf("Invalid arguments\n");
+		exit (EXIT_FAILURE);
+	}
+	context = fn_setup_context(argv);
+	forks = fn_malloc_forks(&context);
 	return (EXIT_SUCCESS);
 }
