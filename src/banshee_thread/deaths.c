e26cc374df370302095adeccd23997ba7e7d37d6
Aurelien Focant <24444885+AurelienFocant@users.noreply.github.com>
2024-11-21 20:57:58 -0300

    deleted self-death checking function which was useless since we got the banshee

--- src/banshee_thread/deaths.c
+++ src/banshee_thread/deaths.c
@@ -15,29 +15,6 @@ bool	fn_check_others_death(t_philo *philo)
 	}
 }
 
-void	fn_check_own_death(t_philo *philo)
-{
-	long	time_now;
-	long	time_diff;
-	long	time_to_die;
-	long	timestamp;
-
-	time_now = fn_get_epoch_in_usec();
-	time_diff = time_now - philo->time_of_last_meal;
-	time_to_die = philo->shared_context->time_to_die * mSEC;
-	if (time_diff >= time_to_die)
-	{
-		pthread_mutex_lock(&(philo->shared_context->death_mutex));
-		philo->shared_context->is_dead[0] = TRUE;
-		philo->shared_context->is_dead[1] = philo->id;
-		pthread_mutex_unlock(&(philo->shared_context->death_mutex));
-		timestamp = fn_get_timestamp(philo);
-		printf("%lu philo %i is dead\n", timestamp, philo->id);
-		fn_unlock_forks(philo);
-		pthread_exit(&(philo->thread));
-	}
-}
-
 void	fn_check_for_deaths(t_philo *philo)
 {
 	if (fn_check_others_death(philo))
